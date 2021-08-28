464a9060a73c960fb222bf187c3c7dafd3a174d4
Jon Stroop <jpstroop@gmail.com>
2021-08-28 14:46:38 -0400

    one successful patch

--- airtable.py
+++ airtable.py
@@ -1,5 +1,7 @@
 from requests import get
+from requests import patch
 from time import sleep
+from sys import stderr
 
 class AirTableAPI():
 
@@ -21,4 +23,20 @@ class AirTableAPI():
         r = get(url, params=params, headers=headers)
         r.raise_for_status()
         # TODO handle non-200s
-        return r.json()
+        payload = r.json()
+        if len(payload['records']) > 100:
+            print('The list of netids is greater than 100; time to implement paging', file=stderr)
+        return payload
+
+    def patch_record(self, body):
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        url = f'{AirTableAPI.API_BASE}{self.app_path}'
+        r = patch(url, json=body, headers=headers)
+        print(r.text)
+        r.raise_for_status()
+
+        # TODO handle non-200s
+        sleep(AirTableAPI.THROTTLE_INTERVAL)
--- main.py
+++ main.py
@@ -1,35 +1,63 @@
-from json import dumps
 from json import load
 from ldap import LDAP_API
 from airtable import AirTableAPI
 
-def load_private(pth='./private.json'):
-    with open(pth, 'r') as f:
-        return load(f)
+def debug_dump(json_payload):
+    from json import dumps
+    print(dumps(json_payload, ensure_ascii=False, indent=2))
+
+class UpdateApp():
+    def __init__(self):
+        private = UpdateApp._load_private()
+        self.airtable = AirTableAPI(private['APP_PATH'], private['AIRTABLE_API_KEY'])
+        self.ldap = LDAP_API(private['LDAP_HOST'], private['LDAP_OC'])
+
+    @staticmethod
+    def _load_private(pth='./private.json'):
+        with open(pth, 'r') as f:
+            return load(f)
+
+    def airtable_patch_data_from_ldap(self, netid, airtable_id):
+        ldap_data = self.ldap.query('uid', netid)
+        return {
+            'records' : [
+                {
+                    'id': airtable_id,
+                    'fields': {
+                        'Name': ldap_data.get('displayName'),
+                        'First Name': ldap_data.get('givenName'),
+                        'Last Name': ldap_data.get('sn'),
+                        'University ID': int(ldap_data.get('universityid')),
+                        'Email': ldap_data.get('mail'),
+                        'University Phone': ldap_data.get('telephoneNumber'),
+                        'Address': ldap_data.get('street')
+                    }
+                }
+            ]
+        }
 
 
+    # TODO: we want:
 
 
 if __name__ == '__main__':
-    private_data = load_private()
-    api_key = private_data['AIRTABLE_API_KEY']
-    app_path = private_data['APP_PATH']
-    ldap_host = private_data['LDAP_HOST']
-    ldap_oc = private_data['LDAP_OC']
+    app = UpdateApp()
+    # get all netids and ids
+    # netids = app.airtable.list_netids()
 
-    # airtable = AirTableAPI(app_path, api_key)
+    # for each, reformat and patch
+    patch_body = app.airtable_patch_data_from_ldap('kl37', 'reciI0wlyLxnEi3GZ')
+    debug_dump(patch_body)
+    app.airtable.patch_record(patch_body)
+
+
+    # app.airtable_patch_data_from_ldap('fkayiwa')
+
+    #
     # netids = airtable.list_netids()
-    # print(dumps(netids, ensure_ascii=False, indent=2))
+    # debug_dump(netids)
 
 
-    ldap = LDAP_API(ldap_host, ldap_oc)
-    data = ldap.query('uid', 'kayiwa')
-    print(dumps(data, ensure_ascii=False, indent=2))
-    # TODO: we want:
-    # First Name (givenName)
-    # Last Name (sn)
-    # netid (should be uid)
-    # Univeristy ID (universityid)
-    # Email (mail)
-    # University Phone (telephoneNumber)
-    # Address (street)
+    # ldap
+    # data = ldap.query('uid', 'kayiwa')
+    # print(dumps(data, ensure_ascii=False, indent=2))
